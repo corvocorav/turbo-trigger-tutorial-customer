fa67b8a6ae5ca82257b788b144324482aecdcd60
corvocorav <corvojaeger@gmail.com>
2024-03-27 03:06:11 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,6 @@
-// Inside your site's HTML template file, typically located in src/theme/your-theme/Layout.js
-
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import { Head } from '@docusaurus/Head'; // Import Head from Docusaurus
+import  { Redirect } from 'react-router-dom';
 
-export default function Layout(props) {
-  return (
-    <>
-      <Head>
-        <meta property="og:title" content="Your Website Title">
-        <meta property="og:description" content="Description of your website">
-        <meta property="og:image" content="https://picsum.photos/200/300">
-
-      </Head>
-      <Redirect to='/docs/Customer script tutorial/intro' />
-    </>
-  );
+export default function Home() {
+  return <Redirect to='/docs/Customer script tutorial/intro' />;
 }
