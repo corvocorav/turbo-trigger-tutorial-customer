d077468a803c81447814e78e25b4feb63eabd96c
corvocorav <corvojaeger@gmail.com>
2024-03-27 03:03:23 +0100

    has erros so idk

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
+// Inside your site's HTML template file, typically located in src/theme/your-theme/Layout.js
+
 import React from 'react';
-import  { Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { Head } from '@docusaurus/Head'; // Import Head from Docusaurus
 
-export default function Home() {
+export default function Layout(props) {
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Your Website Title">
+        <meta property="og:description" content="Description of your website">
+        <meta property="og:image" content="https://example.com/path/to/your-preview-image.png">
 
-  
-  return <Redirect to='/docs/Customer script tutorial/intro' />;
+      </Head>
+      <Redirect to='/docs/Customer script tutorial/intro' />
+    </>
+  );
 }
