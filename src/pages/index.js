709472cf8f6a4b020b23826afe9ed1ed236ee774
corvocorav <corvojaeger@gmail.com>
2024-03-27 02:52:48 +0100

    idk anymore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import  { Redirect } from 'react-router-dom';
-
+import Head from '@docusaurus/Head';
 export default function Home() {
-  return <Redirect to='/docs/Customer script tutorial/intro' />;
+
+  
+  return (
+    <Main>
+
+    <Head>
+    <meta property="og:image" content="image.png" />
+  </Head>
+  
+  <Redirect to='/docs/Customer script tutorial/intro' />
+  </Main>
+  
+  );
 }
